21b11860ca2d9a24a1a637cde8db824fd7f7f243
IsuruRNR <isuru.rnrsolutions@gmail.com>
2024-04-25 20:09:43 +0530

    Added get all, get detail by id methods

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name='apiOverview'),
+    path('product-list/', views.ShowAll, name='product-list'),
+    path('product-detail/<int:pk>/', views.ViewProduct, name='product-detail'),
+    path('product-create/', views.CreateProduct, name='product-create'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
 @api_view(['GET'])
@@ -14,3 +16,24 @@ def apiOverview(request):
     }
 
     return Response(api_urls)
+
+@api_view(['GET'])
+def ShowAll(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def ViewProduct(request,pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def CreateProduct(request):
+    serializer = ProductSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        
+    return Response(serializer.data)
